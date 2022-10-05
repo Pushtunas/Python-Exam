e8178a2c12ef7ad0cd5e72ba6b0c5122bb78e1e8
Edvardas <edvardas.giedrys@codeacademy.lt>
2022-10-05 17:43:41 +0300

    Padaryta 3 užduotis

--- uzduotis3.py
+++ uzduotis3.py
@@ -6,8 +6,28 @@
 # * Kaip argumentą priims žodyną ir grąžins sąrašą su visomis jo reikšmėmis (values).
 
 audi = {
-  "make": 'audi',
-  "model": 'A6',
-  "year": 2005,
-  "color": 'white',
+    "make": 'audi',
+    "model": 'A6',
+    "year": 2005,
+    "color": 'white',
 }
+
+def get_dict_values(zodynas):
+    '''
+    Funkcija pasiima tik reikšmes (values) iš žodyno ir grąžina jų sąrašą
+    Pirmiausiai pasidarome tuščią sarašą kur kelsime reikšmes iš žodyno
+    Tada pasirašom ciklą kuriuo grąžiname supildytą reikšmių sąrašą
+    Iškviečiame 'docstring' tipo komentarą
+    Iškviečiame funkciją ir perduodame norimą žodyną kaip argumentą, kad pamatyti rezultatą dar ir atspausdiname
+    '''
+    sarasas = []
+    for reiksme in zodynas:
+        sarasas.append(zodynas[reiksme])
+    return sarasas
+
+  # jei reiktų atvaizduoti, o ne grąžinti (data prie f-jos iškvietimo nereiktų ir "print")
+  # for reiksme in zodynas:
+    # print(zodynas[reiksme])
+
+print(get_dict_values.__doc__)
+print(get_dict_values(audi))
